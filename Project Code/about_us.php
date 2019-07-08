ea95bb59f05b548950b09c3f96f480dea52e2771
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-07-08 19:22:29 +0600

    Create about_us.php

---
+++ Project Code/about_us.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>About Us</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous"><! Collected from www.bootatrap.com>
+  </head>
+  <body style="width: 1500px;
+               height: 60px;">
+  <body>
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Food For All</a>
+   
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1200px; height: 600px;">
+
+	<div class="content">
+		    <br>
+		    <br>
+			<h1><span class="bold">About Our Organisation</span></h1>
+			<img src="Image/img2.jpg" height="400px" width="1150" style="margin-bottom:10px" /></a>
+      
+       
+	</div>
+</form>
+</div>
+</nav>
+</body>
+</body>
+</html>
