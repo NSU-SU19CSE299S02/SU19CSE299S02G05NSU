38c43a365c78b57db83f7f3f159006165875d102
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-07-21 00:39:05 +0600

    Feature #11 : Donor registration file
    This is the donor registration file.
     Contains
    1. header
    2. nav bar
    3. image

---
+++ Project Code/donor_reg.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Became a Donor</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body style="
+    width: 1500px;
+    height: 70px;
+">
+ <body>
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Food For All</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+ <div style="height:00px;">
+ <from method="post" entype="multipart/from-data" style="width: 1200px; height: 600px;">
+  <div class="content">
+  <br><br><br>
+  <img src="Image/img9.jpg" height="300px" width="1250px" style="margin-bottom:10px"></a>
+  <br><br>
+ </nav>
+</body>
+</body>
+</html>
