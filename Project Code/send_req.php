289c89f07338f346b8343b129ce2d9e58fdee170
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-05 22:08:33 +0600

    Feature #14 : Create send request page
    1.header
    2.nav bar

---
+++ Project Code/send_req.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Send Request for food</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body style="
+    width: 1500px;
+    height: 70px;
+">
+  <body>
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Food For All</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1200px; height: 600px;">
+      <div class="content">
+        <br><br>
