2b3233f51d13a06150a02846006c9524726475e0
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-19 23:21:37 +0600

    Feature #65 : Create donate us page under user
    1. CSS
    2. Navbar
    3. Page heading

---
+++ Project Code/Donor/donate_us.php
@@ -0,0 +1,22 @@
+<html lang="en">
+  <head>
+    <title>Donate Us</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark" style="background-color: #f05656 !important;">
+  <a class="navbar-brand" href="index.php" style="color: #1a1919"><h1>Food For All</h1></a>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="view_req.php" style="color: #1b0252">View Request&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="donate.php" style="color: #1b0252">Donate Food&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="view_donation.php" style="color: #1b0252">View Donation&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="logout.php" style="color: #1b0252">Logout&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="donate_us.php" style="color: #0A122A">Donate Us&ensp;<span class="sr-only">(current)</span></a></li>
+      </ul>
+  </nav>
+</head>
+<body>
+  <div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 700px; background-color:#c6f4f5;">
+      <div class="content"><br>
+  <h1 align="center">Donate us to give us the opportunity to grow more</h1><br><br><br>
