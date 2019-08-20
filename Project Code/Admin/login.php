a5a94cff0261cde2c3368dd58711cc78ab57d65b
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-21 00:05:47 +0600

    Feature #91 : Create login page under admin
    1. CSS
    2. Navbar
    3. box for form

---
+++ Project Code/Admin/login.php
@@ -0,0 +1,18 @@
+<html lang="en">
+  <head>
+    <title>Admin login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark" style="background-color: #4bf2cb !important;">
+  <h1  style="color: #1a1919">Admin Portal</h1>
+    <ul class="navbar-nav ml-auto">
+    </ul>
+</nav>
+</head>
+<body>
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 600px;">
+      <div class="content">
+        <br><br>
+</div>
+<table width="100%" style="margin: 0 auto; border:3px solid;text-align:center">
