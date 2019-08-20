8419d8497b2b431cda1d7c52eabe50c1ce9bb12a
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-20 23:39:37 +0600

    Feature #85 : Create header for map
    1. CSS
    2. Navbar

---
+++ Project Code/Donor/mheader.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Donor Map</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark" style="background-color: #f05656 !important;">
