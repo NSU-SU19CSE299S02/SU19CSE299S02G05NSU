12db9d86d0e6391a11800c643cf4afd4f97a5f50
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-21 00:10:00 +0600

    Feature #91 : Update on admin login
    1. Form
    2. Submit

--- Project Code/Admin/login.php
+++ Project Code/Admin/login.php
@@ -16,3 +16,11 @@
         <br><br>
 </div>
 <table width="100%" style="margin: 0 auto; border:3px solid;text-align:center">
+
+<tr><td class="lefttd"><h3>Username:</h3></td><td><input type="text" name="t1" required="required" title="Please enter your username"/></td></tr>
+<tr><td class="lefttd"><h3>Password:</h3></td><td><input type="password"name="t2"  required="required" pattern="[a-zA-Z0-9]{2,10}" title="please enter only character or numbers between 2 to 10 for password"  /></td></tr>
+
+<tr><td>&nbsp;</td><td><input type="submit" value="Log In" name="sbmt" style="border:0px; background:#2D7FDE; width:125px; height:40px;  box-shadow:2px 2px 10px black; color:#E2ECF8; font-weight:bold; font-size:20px;"></td></tr><br>
+</table>
+
+<?php include 'DBconnection.php'; ?>
