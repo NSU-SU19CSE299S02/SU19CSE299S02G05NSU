9299e0f585b6d6f080db20f90f40b4314b6ba683
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-21 18:54:29 +0600

    Feature #92 : Update on location model
    1. confirm location
    2. get the confirm location

--- Project Code/Donor/locations_model.php
+++ Project Code/Donor/locations_model.php
@@ -30,3 +30,29 @@ function add_location(){
         die('Invalid query: ' . mysqli_error($con));
     }
 }
+function confirm_location(){
+    $con=mysqli_connect ("localhost", 'root', '','foodforall');
+    if (!$con) {
+        die('Not connected : ' . mysqli_connect_error());
+    }
+    $id =$_GET['id'];
+    $confirmed =$_GET['confirmed'];
+    // update location with confirm if admin confirm.
+    $query = "update locations set location_status = $confirmed WHERE id = $id ";
+    $result = mysqli_query($con,$query);
+    echo "Inserted Successfully";
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($con));
+    }
+}
+function get_confirmed_locations(){
+    $con=mysqli_connect ("localhost", 'root', '','foodforall');
+    if (!$con) {
+        die('Not connected : ' . mysqli_connect_error());
+    }
+    // update location with location_status if admin location_status.
+    $sqldata = mysqli_query($con,"
+select id ,lat,lng,description,location_status as isconfirmed
+from locations WHERE  location_status = 1
+  ");
+    
