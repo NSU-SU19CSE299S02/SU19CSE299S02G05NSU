7432ee2030b4bc8c0bd0cb2cb9e4d6ed0a7288d8
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-21 18:08:38 +0600

    Feature #28 : Header for admin page
    1. Css

---
+++ Project Code/Admin/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Welcome</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark" style="background-color: #4bf2cb !important;">
+  <h1 style="color: #1a1919">Admin Portal</h1></a>
