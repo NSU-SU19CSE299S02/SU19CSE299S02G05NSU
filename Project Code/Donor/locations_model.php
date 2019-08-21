d7faa1c3727da9e7eb9ef635164acf519a557c42
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-21 18:49:45 +0600

    Feature #92 : Update on location model
    1. Add db
    2. get inputs from db

--- Project Code/Donor/locations_model.php
+++ Project Code/Donor/locations_model.php
@@ -8,3 +8,25 @@ if(isset($_GET['add_location'])) {
 if(isset($_GET['confirm_location'])) {
     confirm_location();
 }
+function add_location(){
+    $con=mysqli_connect ("localhost", 'root', '','foodforall');
+    if (!$con) {
+        die('Not connected : ' . mysqli_connect_error());
+    }
+    $lat = $_GET['lat'];
+    $lng = $_GET['lng'];
+    $description =$_GET['description'];
+    // Inserts new row with place data.
+    $query = sprintf("INSERT INTO locations " .
+        " (id, lat, lng, description) " .
+        " VALUES (NULL, '%s', '%s', '%s');",
+        mysqli_real_escape_string($con,$lat),
+        mysqli_real_escape_string($con,$lng),
+        mysqli_real_escape_string($con,$description));
+
+    $result = mysqli_query($con,$query);
+    echo"Inserted Successfully";
+    if (!$result) {
+        die('Invalid query: ' . mysqli_error($con));
+    }
+}
