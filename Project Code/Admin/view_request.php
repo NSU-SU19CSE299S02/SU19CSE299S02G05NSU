9b3c45ae308ef31a5363bfe0ac686073805f3bab
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-22 23:38:42 +0600

    Feature #88 : Update view request page under admin
    1.Id
    2.name
    3.Mobile
    4.Email

---
+++ Project Code/Admin/view_request.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 900px; background-color:#ffffff;">
+      <div class="content"><br>
+  <table cellpadding="20" cellspacing="20" width="1000px" height="200px"  style="margin:auto" >
+
+   <tr><td colspan="7" align="center"></td></tr> 
+
+   <tr><td>&nbsp;</td><td>&nbsp;</td></tr>   
+ <tr style="background-color:#7180f5" align="center" class="bold">            
+             <td class="bold" align="center" style="color:#B40404">ID</td><td align="center" style="color:#B40404">Name</td><td align="center"style="color:#B40404">Mobile</td><td align="center" style="color:#B40404">Email</td><td align="center" style="color:#B40404">Location</td><td align="center" style="color:#B40404">Quantity</td>
+        </tr>
+        <?php include 'footer.php'; ?>
