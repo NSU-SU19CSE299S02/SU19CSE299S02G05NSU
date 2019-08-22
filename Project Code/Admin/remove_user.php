e10cc8d90882b3a183fe8c20f3fc84955c00f74c
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-22 20:54:23 +0600

    Feature #74 : Update on remove user from admin page
    1. Select box
    2. Fetch data from db

--- Project Code/Admin/remove_user.php
+++ Project Code/Admin/remove_user.php
@@ -11,6 +11,23 @@ if(isset($_POST["sbmt"]))
 	echo "<script>alert('Record delete');</script>";
 }
 ?>
+<form method="post">
+<table border="0" align="center" width="400" height="300px" class="shaddoww">
+<tr><td colspan="2" align="center" class="toptd" font="8">DELETE USER</td></tr>
+<tr><td colspan="2">&nbsp;</td></tr>
+<tr><td class="lefttd">Select Name</td><td><select name="s2" required><option value="">Select</option>
 
+<?php
+$cn=makeconnection();
+$s="select * from donorregistration";
+	$result=mysqli_query($cn,$s);
+	$r=mysqli_num_rows($result);
+	//echo $r;
+	while($data=mysqli_fetch_array($result))
+	{
+			echo "<option value=$data[1]>$data[1]</option>";	
+	}
+	mysqli_close($cn);
+?>
 
 <?php include 'footer.php'; ?>
