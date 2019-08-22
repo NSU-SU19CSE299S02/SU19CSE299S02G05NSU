e8264dd2fcfb8d766a4d08d8bd5d3ad491268617
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-22 21:02:21 +0600

    Feature #74 : Update on remove user under admin
    1. Get all the data from DB
    2. Submit to delete

--- Project Code/Admin/remove_user.php
+++ Project Code/Admin/remove_user.php
@@ -29,5 +29,36 @@ $s="select * from donorregistration";
 	}
 	mysqli_close($cn);
 ?>
+</select>
 
-<?php include 'footer.php'; ?>
+<?php
+if(isset($_POST["show"]))
+{
+$cn=makeconnection();
+$s="select * from users where f_name='" .$_POST["s2"] ."'";
+	$result=mysqli_query($cn,$s);
+	$r=mysqli_num_rows($result);
+	$data=mysqli_fetch_array($result);
+	$donor_id=$data[0];
+	$f_name=$data[1];
+	$m_name=$data[2];
+	$l_name=$data[3];
+	$mobile=$data[4];
+	$address=$data[5];
+	$email=$data[6];
+	$pwd=$data[7];	
+	mysqli_close($cn);
+}
+?>
+
+</td></tr>
+<tr><td>&nbsp;</td><td><input type="submit" value="DELETE" name="sbmt"></td></tr>
+</table>
+</form>
+</div>
+</div>
+</center>
+<?php include('footer.php'); ?>
+</body>
+</html>
+// Taken help from w3school.com (Remove data from mysql DB)
