3441a818553e8a77c85da647cac0aef5f52adcde
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-22 22:40:35 +0600

    Feature #86 : Update view donation page under admin
    1.Name
    2.Mobile
    3.Request Id

---
+++ Project Code/Admin/view_donation.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 900px; background-color:#ffffff;">
+      <div class="content"><br>
+<table cellpadding="20" cellspacing="20" width="1000px" height="200px"  style="margin:auto" >
+
+   <tr><td colspan="7" align="center"></td></tr> 
+
+   <tr><td>&nbsp;</td><td>&nbsp;</td></tr>   
+   <tr style="background-color:#6247b5" align="center" class="bold">            
+             <td class="bold" align="center" style="color:#ffffff">Name</td><td align="center" style="color:#ffffff">Mobile</td><td align="center"style="color:#ffffff">Email</td><td align="center" style="color:#ffffff">Request ID</td><td align="center" style="color:#ffffff">Food Quantaty(In Kg)</td></tr>
