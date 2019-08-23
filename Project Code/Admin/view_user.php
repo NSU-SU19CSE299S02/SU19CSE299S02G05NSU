11b0659c355595ad698ede695864cb17e8ca670f
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-23 12:54:09 +0600

    Feature #90 : Update view user page under admin
    1.First name
    2.Last name
    3.Mobile
    4.Adress

---
+++ Project Code/Admin/view_user.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 900px; background-color:#ffffff;">
+      <div class="content"><br>
+  <table cellpadding="20" cellspacing="20" width="1000px" height="200px"  style="margin:auto" >
+
+   <tr><td colspan="7" align="center"></td></tr> 
+
+   <tr><td>&nbsp;</td><td>&nbsp;</td></tr>   
+ <tr style="background-color:#7180f5" align="center" class="bold">            
+             <td class="bold" align="center" style="color:#B40404">First Name</td><td align="center" style="color:#B40404">Last Name</td><td align="center"style="color:#B40404">Mobile</td><td align="center" style="color:#B40404">Address</td><td align="center" style="color:#B40404">Email</td></tr>            
