d7603a418eb7d9a4cd9f691461944dc9f2619ae5
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-23 14:08:53 +0600

    Feature #76 : Update user map

---
+++ Project Code/Donor/user_map.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'mheader.php';
+include 'locations_model.php';
+//get_unconfirmed_locations();exit;
+?>
+
+    <script type="text/javascript"
+            src="https://maps.googleapis.com/maps/api/js?language=en&key=AIzaSyCcYTTgVeL9T8aIr9PSVCcZZU8I-89d9L8">
+    </script>
+
+    <div id="map"></div>
+    <script>
+        
