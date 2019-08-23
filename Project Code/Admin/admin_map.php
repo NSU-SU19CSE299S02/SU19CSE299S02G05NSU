5cd57ec04b346df34f2e771ad6d1e1a2b15cf804
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-23 13:03:53 +0600

    Feature #77 : Create admin map
    1. map header
    2. location model
    3. declerar map variables

---
+++ Project Code/Admin/admin_map.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'mheader.php';
+include_once 'locations_model.php';
+?>
+<div id="map"></div>s
+<script>
+    var map;
+    var marker;
+    var infowindow;
+    var red_icon =  'http://maps.google.com/mapfiles/ms/icons/red-dot.png' ;
+    var purple_icon =  'http://maps.google.com/mapfiles/ms/icons/purple-dot.png' ;
+    var locations = <?php get_all_locations() ?>;
