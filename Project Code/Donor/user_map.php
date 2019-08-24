88bb570d74b5a8491383ea750b54dccb7f064785
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-24 21:06:06 +0600

    Feature #76 : Update user map

--- Project Code/Donor/user_map.php
+++ Project Code/Donor/user_map.php
@@ -10,4 +10,23 @@ include 'locations_model.php';
 
     <div id="map"></div>
     <script>
-        
+          /**
+         * Create new map
+         */
+        var infowindow;
+        var map;
+        var red_icon =  'http://maps.google.com/mapfiles/ms/icons/red-dot.png' ;
+        var purple_icon =  'http://maps.google.com/mapfiles/ms/icons/purple-dot.png' ;
+        var locations = <?php get_confirmed_locations() ?>;
+        var myOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(23.791455, 90.400409),
+            mapTypeId: 'roadmap'
+        };
+        map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+        /**
+         * Global marker object that holds all markers.
+         * @type {Object.<string, google.maps.LatLng>}
+         */
+        var markers = {};
